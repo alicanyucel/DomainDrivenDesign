4f8fdfd19330024f5ab55477eb0c8a3cede4920c
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-13 10:45:39 +0300

    domain katmanı

---
+++ DomainDrivenDesign.Domain/Orders/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainDrivenDesign.Domain.Orders;
+
+public sealed class Order
+{
+    public Guid Id { get; set; }
+    public string OrderNumber { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public OrderStatusEnum Status { get; set; }
+    public ICollection<OrderLine> orderLines { get; set; }
+}
