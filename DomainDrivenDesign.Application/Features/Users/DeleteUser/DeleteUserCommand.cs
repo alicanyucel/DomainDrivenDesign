e980b2292434f6e21e261d38a74e142125e10cb9
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-21 17:10:59 +0300

    mola.

---
+++ DomainDrivenDesign.Application/Features/Users/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainDrivenDesign.Application.Features.Users.DeleteUser
+{
+    internal class DeleteUserCommand
+    {
+    }
+}
